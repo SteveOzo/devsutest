f369643e19098bda5d93bacdf1862c909aae07dd
steve <steven-s-47@hotmail.com>
2017-10-20 10:46:54 -0500

    Change options

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 import tornado.escape
 import os.path
-from tornado import gen
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
-from tornado.options import define, options
 import json
+
+from tornado import gen
+from tornado.options import define, options
+
 
 
 define("port", default=8888, help="run on the given port", type=int)
-define("mysql_host", default="127.0.0.1:3306", help="database host")
-define("mysql_user", default="root", help="database user")
-define("mysql_password", default="Chucuri1412", help="database password")
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -21,13 +20,9 @@ class Application(tornado.web.Application):
         ]
 
         settings = dict(
-            page_title=u"Ventas Aliexpress",
-            template_path=os.path.join(os.path.dirname(__file__), "templates"),
-            static_path=os.path.join(os.path.dirname(__file__), "static"),
-            #ui_modules={"Entry": EntryModule},
+            page_title=u"Devsu Test",
             xsrf_cookies=True,
             cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
-            login_url="/",
             debug=True,
         )
         super(Application, self).__init__(handlers, **settings)
